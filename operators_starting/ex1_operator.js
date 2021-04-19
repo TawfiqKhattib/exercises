f56b55f1bd0d1a1f9e8fe11555a2eca83d151c03
tawfiq <tawfiq_kh@yahoo.com>
2021-04-19 13:39:26 +0300

    operator exercises final

--- operators_starting/ex1_operator.js
+++ operators_starting/ex1_operator.js
@@ -22,3 +22,29 @@ let sumNums = (5 + 6) * 3
 console.log(mult)
 console.log(div)
 console.log(sumNums)
+
+// Exercise 5 
+
+console.log((5 > 2) && false)
+console.log(!("knife" === "sword"))
+console.log((1 < 2) || (-1 > -1) || !false)
+console.log("")
+console.log((31 % 5) == "1")
+console.log(!!true)
+console.log("5th Avenue" != "5th Avenue")
+console.log(52 !== "52")
+console.log((undefined || null))
+
+// exercise 6 
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b
+console.log(a)
+console.log(b)
+console.log(c)
