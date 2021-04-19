cb1fe6baae5ee5639ba92d537847e26fcb9dc92e
tawfiq <tawfiq_kh@yahoo.com>
2021-04-19 14:45:50 +0300

    object ex5 commit

--- object_ex/main.js
+++ object_ex/main.js
@@ -26,3 +26,15 @@ let myList = [{ car: "mazda", color: "red" }, { car: "nissan", color: "blue" }]
 console.log("car " + myList[0].car + "  " + "color " + myList[0].color)
 myList.splice(1)
 console.log(myList)
+
+//ex3
+let newList = [{ car: "mitzbishi", color: "yellow" }, { car: "nissan", color: "black" }]
+myList.push(...newList)
+console.log(myList)
+
+//ex4
+let library = {
+    books: [{ title: "book1", auther: "auther1" }, { title: "book2", auther: "auther2" }, { title: "book3", auther: "auther3" }]
+}
+
+//ex5
