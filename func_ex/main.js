e9ce9c5205d80c4708c4aef99a9f836cfd5e2a74
tawfiq <tawfiq_kh@yahoo.com>
2021-04-19 17:24:25 +0300

    function exercises

--- func_ex/main.js
+++ func_ex/main.js
@@ -24,5 +24,62 @@ console.log("you are either " + theAge[0] + " or " + theAge[1])
 function isEven(number) {
     return number % 2 === 0
 }
-let even = isEven(6)
+let even = isEven(5)
 console.log(even)
+
+//ex4
+function printOdd(arrNum) {
+    for (let num of arrNum) {
+        if (!isEven(num)) {
+            console.log(num)
+        }
+    }
+}
+let arrNum = [3, 5, 4, 1, 6, 8, 12, 11]
+printOdd(arrNum)
+
+//ex5
+
+function checkExist(arrNum, num) {
+    for (let number of arrNum) {
+        if (number === num) {
+            return true;
+        }
+    }
+    return false
+}
+let array = [1, 2, 3, 4]
+let isExist = checkExist(array, 5)
+console.log(isExist)
+
+//ex6
+let calculator = {
+    add: function(fNum, sNum) {
+        return fNum + sNum
+    },
+    subtract: function(fNum, sNum) {
+        return fNum - sNum
+    }
+}
+const result1 = calculator.add(20, 1)
+const result2 = calculator.subtract(30, 9)
+
+console.log(calculator.add(result1, result2)) //should print 42
+
+//ex7
+function increaseByNameLength(money, name) {
+    return money * (name.length)
+}
+
+function makeRegal(name) {
+    return "His Royal Highness, " + name
+}
+const turnToKing = function(name, money) {
+    name = name.toUpperCase()
+    money = increaseByNameLength(money, name)
+    name = makeRegal(name)
+
+    console.log(name + " has " + money + " gold coins")
+}
+
+turnToKing("martin luther", 100) // should print "His Royal Highness, MARTIN LUTHER has 1300 gold coins"
