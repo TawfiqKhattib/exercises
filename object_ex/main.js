7c63fd52f16d635c33a8c64048137a52a0843efa
tawfiq <tawfiq_kh@yahoo.com>
2021-04-19 14:37:47 +0300

    object commit

---
+++ object_ex/main.js
@@ -0,0 +1,21 @@
+//ex1
+let p1 = {
+    name: "first",
+    age: 22,
+    city: "ghajar"
+}
+let p2 = {
+    name: "second",
+    age: 22,
+    city: "kryat shemona"
+}
+
+if (p1.age === p2.age) {
+    console.log("woww!!")
+}
+
+if (p1.city === p2.city) {
+    console.log("Jill wanted to date Robert")
+} else {
+    console.log("Jill wanted to date Robert, but couldn")
+}
