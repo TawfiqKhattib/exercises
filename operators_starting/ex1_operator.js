c34beb74f4afc0dda44a4573da1d00eca3cc52ac
tawfiq <tawfiq_kh@yahoo.com>
2021-04-19 13:36:01 +0300

    operator exercises

---
+++ operators_starting/ex1_operator.js
@@ -0,0 +1,24 @@
+const maxCapacity = 14
+let surfTime = true
+let bestStudent
+const purposInLife = ''
+
+// Exercises 2
+
+let myString = "js "
+let result = myString + "is the best thing ever"
+console.log(result)
+
+// Exercise 3
+let password = "1234"
+let confirmPassword = "1234"
+console.log(password === confirmPassword)
+
+//exercise 4
+
+let mult = 423 * 12
+let div = 802 / 2
+let sumNums = (5 + 6) * 3
+console.log(mult)
+console.log(div)
+console.log(sumNums)
