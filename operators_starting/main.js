15d776c818e43a88859f6eecee917af55c9fdda9
tawfiq <tawfiq_kh@yahoo.com>
2021-04-19 13:22:22 +0300

    first operator

---
+++ operators_starting/main.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!!")
+console.log(((4 + 5) > 10) && !("a" == "b")) // we'll see what all this means soon
+const country = "Djibouti"
+console.log(country)
+const shoobeedoo = "Djibouti"
+console.log(shoobeedoo)
+const name = "Taylor" //initialized with the value of "Taylor"
+let shoppingCartItems //not initialized, only defined - we'll learn about let in a second
+console.log(shoppingCartItems)
