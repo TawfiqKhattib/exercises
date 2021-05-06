c53c388f1a41d702a0c37bb8e52460ddfe32fe82
tawfiq <tawfiq_kh@yahoo.com>
2021-05-06 11:23:24 +0300

    oop inheritance

--- myPeople_ajax/main.js
+++ myPeople_ajax/main.js
@@ -1,13 +1,18 @@
+const arrUsers = [];
 const relevantData = function(data) {
     let users = {};
-    users.name = data.results[0].name.title + " " + data.results[0].name.first + " " + data.results[0].name.last;
-    users.email = data.results[0].email;
-    setHtml(users);
+    for (let index in data.results) {
+        users.name = data.results[index].name.title + " " + data.results[index].name.first + " " + data.results[index].name.last;
+        users.email = data.results[index].email;
+        arrUsers.push(users);
+        users = {};
+    }
+    setHtml(arrUsers);
 }
 const user = function() {
     $.ajax({
         method: "GET",
-        url: 'https://randomuser.me/api/',
+        url: 'https://randomuser.me/api/?results=10',
         dataType: 'json',
         success: relevantData,
         error: function(xhr, text, error) {
@@ -16,18 +21,18 @@ const user = function() {
     });
 }
 
-const setHtml = function(person) {
+const setHtml = function(persons) {
     const source = $('#menu-template').html();
     const template = Handlebars.compile(source);
-    const newHTML = template(person);
+    const newHTML = template({ person: persons });
     $('#container').append(newHTML);
 }
 
-const addUser = function() {
-    for (let i = 0; i < 10; i++) {
-        user()
-    }
-}
+// const addUser = function() {
+//     for (let i = 0; i < 10; i++) {
+//         user()
+//     }
+// }
 
 $("#container").on("mouseover", ".person", function() {
     $(this).css("background-color", "rgb(150, 150, 192)");
@@ -37,4 +42,5 @@ $("#container").on("mouseout", ".person", function() {
     $(this).css("background-color", "#d2dadf");
 });
 
-addUser();
+// addUser();
+user();
