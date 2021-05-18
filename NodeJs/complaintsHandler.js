4b3702aefd5d996f5a7f97eafc4d9935c6188859
tawfiq <tawfiq_kh@yahoo.com>
2021-05-18 12:52:26 +0300

    NodeJs

---
+++ NodeJs/complaintsHandler.js
@@ -0,0 +1,13 @@
+let consts = require("./consts")
+const handleComplaints = function(complaint) {
+    if (complaint.type === consts.FINANCE) {
+        console.log("Money doesn’t grow on trees, you know.");
+    } else if (complaint.type === consts.WEATHER) {
+        console.log("It is the way of nature. Not much to be done.");
+    } else if (complaint.type === consts.EMOTIONS) {
+        console.log("It’ll pass, as all things do, with time.");
+    } else {
+        console.log("you'r input have to be : finance or weather or emotions ");
+    }
+}
+module.exports.handleComplaints = handleComplaints
